1c10d0e6154f5cbad924f89ecb3888a9f95d26b4
unqua <332755351@qq,com>
2022-09-16 16:42:47 +0800

    add config files for dann

---
+++ train/dann/hyrank.bat
@@ -0,0 +1,15 @@
+call "E:\zts\software\Anaconda\Scripts\activate.bat" zts
+cd E:\zts\HSI_domain_adaption
+set PYTHONPATH=%cd%
+
+rem dann
+python train/dann/train.py configs/hyrank/dann.yaml ^
+        --path ./runs/hyrank/dann-train ^
+        --nodes 1 ^
+        --gpus 1 ^
+        --rank-node 0 ^
+        --backend gloo ^
+        --master-ip localhost ^
+        --master-port 8890 ^
+        --seed 30 ^
+        --opt-level O1
---
+++ train/dann/shanghang.bat
@@ -0,0 +1,15 @@
+call "E:\zts\software\Anaconda\Scripts\activate.bat" zts
+cd E:\zts\HSI_domain_adaption
+set PYTHONPATH=%cd%
+
+rem dann
+python train/dann/train.py configs/shanghang/dann.yaml ^
+        --path ./runs/shanghang/dann-train ^
+        --nodes 1 ^
+        --gpus 1 ^
+        --rank-node 0 ^
+        --backend gloo ^
+        --master-ip localhost ^
+        --master-port 8890 ^
+        --seed 30 ^
+        --opt-level O1
