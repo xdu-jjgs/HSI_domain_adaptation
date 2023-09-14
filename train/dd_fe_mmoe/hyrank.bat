e53cc9f31132ba265fd892e6fe9c84c753364626
unqua <332755351@qq,com>
2023-09-14 15:30:20 +0800

    fix bugs in dd_fe_mmoe

--- train/dd_fe_mmoe/hyrank.bat
+++ train/dd_fe_mmoe/hyrank.bat
@@ -2,9 +2,9 @@ call "E:\zts\software\Anaconda\Scripts\activate.bat" zts
 cd E:\zts\HSI_domain_adaptation
 set PYTHONPATH=%cd%
 
-rem dd_mmoe
-python train/dd_mmoe/train.py configs/hyrank/dd_mmoe/dd_mmoe.yaml ^
-        --path ./runs/hyrank/dd_mmoe-train ^
+rem dd_fe_mmoe
+python train/dd_fe_mmoe/train.py configs/hyrank/dd_fe_mmoe/dd_fe_mmoe.yaml ^
+        --path ./runs/hyrank/dd_fe_mmoe-train ^
         --nodes 1 ^
         --gpus 1 ^
         --rank-node 0 ^
